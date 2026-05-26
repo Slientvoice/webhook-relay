cae732a84fe2356fb58e1e75482e4dfdfb29c95d
Crab <jiamaooooo@gmail.com>
2026-05-26 15:53:02 +0800

    Revert "add version check endpoint ?action=v"
    This reverts commit d7944e1c1b5725069eb166888aa9e376f7007925.

--- api/webhook.js
+++ api/webhook.js
@@ -117,9 +117,6 @@ module.exports = async function handler(req, res) {
       if (a === 'stats') {
         return res.json({ pending: pending.size });
       }
-      if (a === 'v') {
-        return res.json({ v: '2.0-direct', commit: '6ebf5e6' });
-      }
       return res.json({ ok: true });
     }
 
